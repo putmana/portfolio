3a55e7b6a3a4e9b9b8fa9987ee9c1f9b3c62ad51
putmana <adam@ptmn.io>
2023-08-01 16:31:23 -0600

    Added tags and placeholder image for project cards

--- src/lib/components/cards/project-card.svelte
+++ src/lib/components/cards/project-card.svelte
@@ -1,18 +1,23 @@
 <script lang=ts>
     export let header: string;
-    export let hue: string = "151";
     export let image: string = "";
     export let buttonLink: string | null = null;
     export let buttonLabel: string | null = null;
     export let reversed: boolean = false;
     export let alt: string = "";
+    export let tag: string | null = null;
 
 </script>
 
-<div class="card" style="--hue: {hue}" class:reversed>
+<div class="card" class:reversed>
     <div class="info">
         <div class="header">
             {header}
+            {#if tag !== null}
+                <div class="tag">
+                    <i class="bi bi-cone-striped"></i> {tag}
+                </div>
+            {/if}
         </div>
         <div class="body">
             <slot/>
@@ -36,6 +41,7 @@
     .card {
         display: flex;
         flex-direction: row;
+        flex-grow: 1;
         color: lib.$color-accent-a;
         border-color: lib.$color-accent-a;
         background-color: lib.$color-bg-b;
@@ -49,6 +55,18 @@
                 font-size: 30pt;
                 font-weight: 600;
                 letter-spacing: -1px;
+                align-items: center;
+                .tag {
+                    font-weight: 500;
+                    letter-spacing: 0px;
+                    font-size: 10pt;
+                    margin-left: 15px;
+                    margin-right: 15px;
+                    border: 3px dotted lib.$color-accent-a;
+                    padding: 8px;
+                    padding-left: 8px;
+                    padding-right: 8px;
+                }
             }
             .body {
                 display: flex;
@@ -58,7 +76,7 @@
                 padding-left: 30px;
                 font-size: 14pt;
                 font-weight: 500;
-                border-left: 3px dotted hsl(var(--hue), lib.$sat-accent, lib.$lig-accent-a);
+                border-left: 3px dotted lib.$color-accent-a;
             }
             .footer {
                 display: flex;
@@ -111,8 +129,12 @@
             .info {
                 padding: 30px;
                 .header {
+                    flex-direction: column;
                     font-size: 20pt;
                     justify-content: center;
+                    .tag {
+                        margin-top: 10px;
+                    }
                 }
                 .body {
                     font-size: 12pt;
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -10,15 +10,27 @@
         </BannerCard>
     </div>
     <div class="row">
-        <ProjectCard hue="30" header="Marigold" image="/images/marigold.png" alt="Screenshot of Marigold" buttonLink="https://marigoldmusic.live" buttonLabel="Check out the demo">
+        <ProjectCard header="Marigold" image="/images/marigold.png" alt="Screenshot of Marigold" buttonLink="https://marigoldmusic.live" buttonLabel="Check out the demo" tag="Under Construction">
             <p>A full-stack music streaming app that puts you in the driver's seat. Upload your own music and enjoy the convenience of streaming apps while retaining control of your library. By far the coolest thing I've built (yet...)</p>
             <p>Built with <b>SvelteKit, SCSS, TypeScript, Laravel, and MySQL</b></p>
         </ProjectCard>
     </div>
     <div class="row">
-        <ProjectCard hue="30" header="My Old Website" image="/images/old-website.jpeg" alt="Screenshot of old website" buttonLink="https://marigoldmusic.live" buttonLabel="Check out the demo" reversed>
+        <ProjectCard header="My Old Website" image="/images/old-website.jpeg" alt="Screenshot of old website" buttonLink="https://marigoldmusic.live" buttonLabel="Check it out" reversed>
             <p>An older version of my portfolio website I built in college.</p>
             <p>Built with <b>Bootstrap and jQuery</b></p>
+        </ProjectCard>
+    </div>
+    <div class="row">
+        <ProjectCard header="Stargaze" image="/images/coming-soon.png" alt="Coming soon" tag="Coming Soon">
+            <p>An API-based weather app with a beautiful pixel art style.</p>
+            <p>Built with <b>SvelteKit, SCSS, and TypeScript</b></p>
+        </ProjectCard>
+    </div>
+    <div class="row">
+        <ProjectCard header="RentAssist" image="/images/coming-soon.png" alt="Coming soon" tag="Coming Soon" reversed>
+            <p>A simple application to help roomates split rent and other expenses on a monthly basis.</p>
+            <p>Built with <b>Svelte, Bootstrap, TypeScript, and PocketBase</b></p>
         </ProjectCard>
     </div>
     
