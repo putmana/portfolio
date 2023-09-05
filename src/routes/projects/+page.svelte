347e2dca85c9f22a2052dc27ca18d2024bfbd5fe
putmana <adam@ptmn.io>
2023-09-05 02:05:11 -0600

    Added B-Sides to portfolio

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -17,30 +17,32 @@
         </BannerCard>
     </div>
     <div class="row" style="--offset-time: {2 * animTime}s">
-        <ProjectCard header="Marigold" image="/images/marigold.png" alt="Screenshot of Marigold" buttonLink="https://marigoldmusic.live/auth/login" buttonLabel="Check out the demo" tag="Full Stack">
+        <ProjectCard header="Marigold" image="/images/marigold.png" alt="Screenshot of Marigold" buttonLink="https://marigoldmusic.live/auth/login" buttonLabel="Check it out" tag="Full Stack">
             <p>A demo of a full-stack desktop music streaming app I built as my college capstone project. The idea is to allow users to upload their own music tracks and play them back from here, providing the convenience of music streaming with the control of an MP3 player.</p>
             <p>Built with <b>SvelteKit, SCSS, TypeScript, Laravel, and MySQL</b></p>
         </ProjectCard>
     </div>
     <div class="row" style="--offset-time: {3 * animTime}s">
-        <ProjectCard header="Skygaze" image="/images/stargaze.png" alt="Coming soon" tag="Fully Responsive" buttonLink={"https://skygaze.ptmn.io"} buttonLabel="Check it out" reversed>
+        <ProjectCard header="Skygaze" image="/images/stargaze.png" alt="Screenshot of Stargaze" tag="Fully Responsive" buttonLink={"https://skygaze.ptmn.io"} buttonLabel="Check it out" reversed>
             <p>A fully responsive weather app with a unique pixel art style designed and built entirely by me. I even made the icons. Uses the OpenWeatherMap API to find the weather near you. Built to be a fun project to put on my portfolio.</p>
             <p>Built with <b>SvelteKit, SCSS, and TypeScript</b></p>
         </ProjectCard>
     </div>
     <div class="row" style="--offset-time: {4 * animTime}s">
-        <ProjectCard header="RentAssist" image="/images/coming-soon.png" alt="Coming soon" tag="Full Stack & Fully Responsive" buttonLink={"https://rentassist.ptmn.io"} buttonLabel="Coming Soon" disabled>
-            <p>A website to help roomates split rent and other expenses. Initially built as an extremely simple calculator to help me split my rent with my roomates, I saw an opportunity to turn this into a real project.</p>
-            <p>Built with <b>Svelte, Bootstrap, TypeScript, and PocketBase</b></p>
+        <ProjectCard header="B-Sides" image="/images/bsides.png" alt="Screenshot of B-Sides" tag="Full Stack & Fully Responsive" buttonLink={"https://bsides.ptmn.io"} buttonLabel="Check it out">
+            <p>A simple responsive board-based image forum website. This was originally a college project I built during my freshman year, but I've made some major modifications under the hood to make it more modular.</p>
+            <p>Built with <b>Flask, Bootstrap, and MySQL</b></p>
         </ProjectCard>
     </div>
     <div class="row" style="--offset-time: {5 * animTime}s">
-        <ProjectCard header="Store Page" tag="College Project" buttonLink={"https://demo.ptmn.io/store-page"} buttonLabel="Check out the demo">
-            <p>A responsive demo of a store page I built in college. Features a review section, different product variants, and an "add to cart" feature.</p>
-            <p>Built with <b>Vue and Bootstrap</b></p>
+        <ProjectCard header="RentAssist" image="/images/coming-soon.png" alt="Coming soon" tag="Full Stack & Fully Responsive" buttonLink={"https://rentassist.ptmn.io"} buttonLabel="Coming Soon" reversed disabled>
+            <p>A website to help roomates split rent and other expenses. Initially built as an extremely simple calculator to help me split my rent with my roomates, I saw an opportunity to turn this into a real project.</p>
+            <p>Built with <b>Vue, Bootstrap, TypeScript, and PocketBase</b></p>
         </ProjectCard>
-        <ProjectCard header="Grocery List" tag="College Project" buttonLink={"https://demo.ptmn.io/grocery-list"} buttonLabel="Check out the demo">
-            <p>A demo of a responsive checklist app. You can add and remove items, change their order, and mark them as important.</p>
+    </div>
+    <div class="row" style="--offset-time: {6 * animTime}s">
+        <ProjectCard header="Grocery List" image="/images/grocery-list.png" alt="Screenshot of grocery list website" tag="Fully Responsive" buttonLink={"https://demo.ptmn.io/grocery-list"} buttonLabel="Check it out">
+            <p>The obligatory checklist app. You can add and remove items, change their order, and mark them as important. I built this a long time ago, but I'm still pretty happy with how the styling turned out.</p>
             <p>Built with <b>Vue and Bootstrap</b></p>
         </ProjectCard>
     </div>
